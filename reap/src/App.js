39bcecc1c6a090f6c5b72ad5167e023d1797a178
Bryuhn <bryuhn@gmail.com>
2023-08-29 15:26:43 +0200

    recapcha

--- reap/src/App.js
+++ reap/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import reCAPTCHA from "react-google-recaptcha"
 
 function App() {
   return (
-    <div>hello this is an app updated!""
-
+    <div>
+        <form>
+          <label htmlFor="name">Name</label>
+              <input type="text" id="name" className="input"/>
+              <reCAPTCHA
+              sitekey={process.env.REACT_APP_SITE_KEY}
+               />
+          <button>Submit</button>
+      </form>
     </div>
   );
 }
